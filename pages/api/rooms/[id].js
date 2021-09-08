e3be58dfb9b2b045bdd84100acd0d74cf72a2952
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-09-08 12:07:27 +0100

    Passing the error middleware to next-connect

--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,8 +1,9 @@
 import nc from "next-connect";
 import {getSingleRoom, updateRoom, deleteRoom} from "../../../controllers/roomControllers";
 import connectDB from "../../../db/connectDB";
+import onError from "../../../middlewares/errors";
 
-const handler = nc();
+const handler = nc({onError});
 
 connectDB()
 
--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,10 +1,12 @@
 import nc from "next-connect";
 import {getAllRooms, createRoom, getSingleRoom} from "../../../controllers/roomControllers";
 import connectDB from "../../../db/connectDB";
+import onError from "../../../middlewares/errors";
 
-const handler = nc();
+const handler = nc({onError});
 
 connectDB()
+
 handler.get(getAllRooms, getSingleRoom);
 handler.post(createRoom);
 
