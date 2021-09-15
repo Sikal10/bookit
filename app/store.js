b70a4dbf38b9de1641b0ff677de8c0b300cd6340
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-09-15 16:14:41 +0100

    Redux toolit added

---
+++ app/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+import roomsReducer from "../slices/roomSlices/roomSlice";
+import authReducer from "../slices/authSlices/authSlice";
+
+export const store = configureStore({
+    reducer: {
+        rooms: roomsReducer,
+        auth: authReducer
+    }
+})
